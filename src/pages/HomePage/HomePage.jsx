56dd8bde36831bd306619e48c35c0c0f56c1a127
Sayed-Udoy <umudoy364@gmail.com>
2025-03-31 21:10:24 -0700

    post feed creating done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
+import Feedposts from "@/components/FeedPosts/Feedposts"
 import { ColorModeButton } from "@/components/ui/color-mode"
+import { Box, Container, Flex } from "@chakra-ui/react"
 
 const HomePage = () => {
   return (
-    <div>
-      home
-    </div>
+    <Container maxW={"container.lg"}>
+      <Flex gap={20} >
+        <Box flex={2} py={10} >
+          <Feedposts />
+        </Box>
+        <Box flex={3} mr={20} display={{base:"none",md:"block"}} maxW={"300px"} border={"1px solid blue"} >
+          Suggested
+        </Box>
+      </Flex>
+    </Container>
   )
 }
 
