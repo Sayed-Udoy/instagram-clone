3a9d92d64b64bb8b4193f611562bc2c5480b6d83
Sayed-Udoy <umudoy364@gmail.com>
2025-03-31 21:53:41 -0700

    postfotter done

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { CommentLogo, NotificationsLogo, UnlikeLogo } from "@/assets/contants";
+import { Box, Button, Flex, Group, Input, Span, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { InputGroup } from "../ui/input-group";
 
 const PostFooter = () => {
-  return (
-    <div>PostFooter</div>
-  )
-}
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
 
-export default PostFooter
+  const handleLike = () => {
+    if (liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  };
+
+  return (
+    <>
+      <Flex alignItems={"center"} gap={4} w="full" pt={0} mb={2} mt={2}>
+        <Box onClick={handleLike} cursor={"pinter"} fontSize={18}>
+          {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
+        </Box>
+        <Box fontSize={18}>
+          <CommentLogo />
+        </Box>
+      </Flex>
+      <Text fontWeight={600} fontSize={"sm"}>
+        {likes} likes
+      </Text>
+      <Text fontWeight={700} fontSize={"sm"}>
+        as a programmer
+        <Text as={Span} fontSize={"sm"} color={"gray"}>
+          Feeling good
+        </Text>
+      </Text>
+      <Text color={"gray"} fontSize={"sm"}>
+        View all 1,000 comments
+      </Text>
+      <Flex
+        gap={2}
+        alignItems="center"
+        justifyContent={"space-between"}
+        w="full"
+      >
+        <Group w="full" attached >
+          <Input
+          flex={1}
+            variant={"flushed"}
+            placeContent={"Add a comment...."}
+            fontSize={14}
+          />
+          <Button variant="outline" px={2} py={2}>
+            Post
+          </Button>
+        </Group>
+      </Flex>
+    </>
+  );
+};
+
+export default PostFooter;
--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,8 +1,27 @@
+import { Avatar, Box, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const PostHeader = () => {
   return (
-    <div>PostHeader</div>
+    <Flex justifyContent={"space-between"} alignItems={"center"} w="full" my={2}>
+        <Flex alignItems={"center"} gap={2} >
+            <Image src='/img1.png' alt='uer profile pic' w="40px" h={"40px"} rounded="full" />
+            <Flex fontSize={12} fontWeight={"bold"} gap={2} >
+                as programmer
+                <Box color={"gray.500"} >
+                    .1w
+                </Box>
+            </Flex>
+        </Flex>
+        <Box cursor="pointer" >
+            <Text fontSize={12} color={"blue.500"} fontWeight={"bold"} _hover={{
+                color: "white"
+            }} transition={"0.2s ease-in-out"}  >
+                Unfollow
+            </Text>
+        </Box>
+    </Flex>
+
   )
 }
 
